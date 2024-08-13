54db645c00037aaeb94361e9b00728aa4f1cc27b
kallwaa <2028260566@qq.com>
2024-08-13 14:33:20 +0800

    首次master提交

---
+++ gitStudy.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class gitStudy {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            strings.add(String.valueOf(i));
+        }
+    }
+}
