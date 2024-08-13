4d63705ea077cd74da1e430bbfe6d0e4d9da847e
kallwaa <2028260566@qq.com>
2024-08-13 14:43:50 +0800

    master分支的第一次提交

--- gitStudy.java
+++ gitStudy.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class gitStudy {
     public static void main(String[] args) {
@@ -7,5 +9,6 @@ public class gitStudy {
         for(int i=0;i<10;i++){
             strings.add(String.valueOf(i));
         }
+        Set<String> set =new HashSet<>();
     }
 }
